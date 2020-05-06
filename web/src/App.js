1033d4648d699f0421a40a6ac3c663766071a0d5
marquebelem <camilambdasilva@gmail.com>
2020-05-06 16:28:24 -0300

    atualizando com a ultima atividade do curso

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from "./components/Header";
+import Main from "./pages/main";
 
 const App = () => (
   <div className="App">
     <Header/>
+    <Main/>
   </div>
 );
 
